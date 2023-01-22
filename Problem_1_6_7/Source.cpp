758b1f1f775431b3fac820803ae21c807b11ee42
Admin <dashko.pavel@bk.ru>
2023-01-23 00:37:19 +0300

    The problem 1_6_7 is starting

---
+++ Problem_1_6_7/Source.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+// Процентная ставка по вкладу составляет P процентов годовых, которые прибавляются 
+// к сумме вклада через год.Вклад составляет X рублей Y копеек.Определите размер вклада 
+// через K лет. Программа должна вывести два числа : величину вклада через K лет в рублях 
+// и копейках.Дробное число копеек по истечение года отбрасывается.
+// Перерасчет суммы вклада (с отбрасыванием дробных частей копеек) происходит ежегодно.
+// Тесты.
+// 12 179 0 5 -> 315 43
+//
+//
+int main()
+{
+	int p = 0, x = 0, y = 0, rubl_incop = 0, sumkop = 0, sumkop_with_proc = 0, rubl = 0, kop = 0;
+	double sum_incop_proc = 0;
+	cin >> p >> x >> y;
+	rubl_incop = 100 * x;
+	sumkop = rubl_incop + y;
+	sum_incop_proc = sumkop * p / 100; // преоразование int в тип double (безопасно по Страуструп Б.)
+	sumkop_with_proc = sum_incop_proc + sumkop; // преобразование double в int (опасно ппц)
+	kop = sumkop_with_proc % 100;
+	rubl = (sumkop_with_proc - kop) / 100;
+	cout << rubl << " " << kop << endl << "New step for my view";
+	cout << "Hello its me";
+	return 0;
+
+}
