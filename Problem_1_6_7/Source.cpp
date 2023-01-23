001011285862a66d7dc4b6f50bbe6e42f44229c0
Admin <dashko.pavel@bk.ru>
2023-01-23 20:31:17 +0300

    This version is right, but there is a loss of precision

--- Problem_1_6_7/Source.cpp
+++ Problem_1_6_7/Source.cpp
@@ -12,20 +12,21 @@ using namespace std;
 //
 int main()
 {
-	int p = 0, x = 0, y = 0, k = 0, i = 0, rubl_incop = 0, sumkop = 0, sumkop_with_proc = 0, rubl = 0, kop = 0;
+	double p = 0, x = 0, y = 0, k = 0, rubl_incop = 0, sumkop = 0, sumkop_with_proc = 0, rubl = 0, kop = 0;
 	double perem = 0;
 	double proc = 0;
+	int i = 0;
 	cin >> p >> x >> y >> k;
 	rubl_incop = 100 * x;
 	sumkop = rubl_incop + y;
 	proc = p / 100;
 	while ( i < k )
 	{ 
-		double perem = sumkop * proc + sumkop;
-		double sumkop = perem;
+		perem = sumkop * proc + sumkop;
+		sumkop = perem;
 	    ++i;
 	}
-	cout << rubl << " " << kop;
+	cout << perem;
 	return 0;
 
 }
