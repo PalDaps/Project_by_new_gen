9d25f12237ae35ac303df8f54b89a26d3339c033
Admin <dashko.pavel@bk.ru>
2023-01-23 23:38:08 +0300

    The problem 1_6_8 was solved

--- Problem_1_6_8/Source.cpp
+++ Problem_1_6_8/Source.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
-#include <cmath>
+#include <iomanip>
 using namespace std;
-// Определите среднее значение всех элементов последовательности, завершающейся числом 0. 
-// Тесты.
-// 1 7 9 0 -> 5.66666666667
-
 int main()
 {
-	int vvod = 0, after = 0, sum = 0;
+	int vvod = 0, after = 0, sum = 0, counter = 0;
+	double average_v = 0;
+	double sum_d = 0;
+	double counter_d = 0;
 	cin >> vvod;
-	while ( vvod != 0 )
+	while (vvod != 0)
 	{
 		sum = vvod + after;
-		after = vvod;
+		after = sum;
+		++counter;
 		cin >> vvod;
 	}
+	sum_d = sum;
+	counter_d = counter;
+	average_v = sum_d / counter_d;
+	cout << setprecision(11) << fixed;
+	cout << average_v;
+	return 0;
 }
