68c276e6867f822e90bd755b8c4b7b52259acb9f
Admin <dashko.pavel@bk.ru>
2023-01-24 18:45:58 +0300

    This was interesting problem. I understand some usefull properties of algorithms in a while loop. Problem 1_6_10 was solved. Nice. Step by step!!!

--- Problem_1_6_10/Source.cpp
+++ Problem_1_6_10/Source.cpp
@@ -11,22 +11,28 @@ int main()
 	int n = 0, c = 0, i = 0;
 	double x = 0, a = 0, form_1 = 0, form_2 = 0, next_a = 0;
 	cin >> n >> x >> a;
-	form_1 = a;
-	while ( i < n )
-	{
-		cin >> a;
-		form_2 = form_1 + a;
-		++i;
-		form_1 = (form_1 * x + a) * x;
-		
-	}
-	if ( n == 1 )
+	cout << setprecision(2) << fixed;
+	if ( n != 0 ) 
 	{ 
-	cout << form_1 + a;
+		form_1 = a;
+		while ( i < n )
+		{
+		cin >> a;
+		++i;
+		form_1 = form_1 * x + a;
+		}
+		if ( n == 1 )
+		{
+		cout << form_1;
+	    }
+	    else
+	    {
+		cout << form_1;
+	    }
 	}
-	else
+	else if (n == 0)
 	{
-		cout << form_2;
+		cout << a;
 	}
 	return 0;
 }
