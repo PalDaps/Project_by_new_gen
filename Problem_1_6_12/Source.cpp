16ad5d98c24f2b8240dac7333d30ad9d4fcfe432
Admin <dashko.pavel@bk.ru>
2023-01-25 20:38:09 +0300

    The first version of problem 1_6_12

---
+++ Problem_1_6_12/Source.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	double a = 0, b = 0, c = 0, d = 0, x = 0, x1 = 0, x2 = 0;
+		cin >> a >> b >> c;
+		if (a == 0 && b == 0 && c == 0)
+		{
+			cout << 3;
+		}
+		d = pow(b, 2) - 4 * a * c;
+		if (d == 0 && a != 0 && b !=0 && c != 0)
+		{
+			x = -b / (2 * a);
+			cout << 1 << " " << x;
+		}
+		else if (d > 0)
+		{
+			x1 = -b / (2 * a) - sqrt(d) / (2 * a);
+			x2 = -b / (2 * a) + sqrt(d) / (2 * a);
+			if (x1 > x2)
+			{
+				cout << 2 << " " << x2 << " " << x1;
+			}
+			else
+			{
+				cout << 2 << " " << x1 << " " << x2;
+			}
+		}
+		else if ( d < 0 )
+		{
+			cout << 0;
+		}
+	return 0;
+}
