a4ccf3fbc1406f97086a8d0edf96cea30dca49a2
Admin <dashko.pavel@bk.ru>
2023-01-30 18:41:22 +0300

    Own TBS 1_7_10. I figured out what ( auto i : a) is and what it is equivalent to, I’ll also use the boolean type now

--- Problem_1_7_10_TBS/Source.cpp
+++ Problem_1_7_10_TBS/Source.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int main() {
-    int n, t, v;
+int main()
+{
+    int n = 0, c = 0, b = 0, temp = 0;
     cin >> n;
-    vector <int> a;
-    // this is only the reading stage
-    for (int i = 0; i < n; i++) 
+    vector<int> pepega;
+    for (int i = 0; i < n; ++i)
     {
-        cin >> t;
-        v = 1;
-        for (auto j : a) if (j == t) {
-            v--;
-            break;
+        c = 1;
+        cin >> temp;
+        for (int i = 0; i < pepega.size(); ++i)
+        {
+            if (pepega[i] == temp)
+            {
+                c--;
+            }
+
         }
-        if (v) a.push_back(t); // if 1 then true and executed, if 0 then false and not executed
+        if (c)
+        {
+            pepega.push_back(temp);
+        }
     }
-    // genius solutin!
-    cout << a.size();
+    cout << pepega.size();
     return 0;
 }
