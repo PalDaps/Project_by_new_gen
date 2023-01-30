07e2f6e3422152eb0f1217434639a871435b0bb8
Admin <dashko.pavel@bk.ru>
2023-01-30 19:23:30 +0300

    TBS 1_7_10 Understood with the break statement. As soon as the condition is fulfilled and in the body of the loop all instructions have been executed, it leaves the if, else, else if, which are connected.

--- Problem_1_7_10_TBS/Source.cpp
+++ Problem_1_7_10_TBS/Source.cpp
@@ -3,32 +3,27 @@
 using namespace std;
 int main()
 {
-    int n = 0, b = 0, temp = 0;
-    bool c = true; // flag
-    cin >> n;
-    vector<int> pepega;
-    for (int i = 0; i < n; ++i)
-    {
-        cin >> temp;
-        for (int i = 0; i < pepega.size(); ++i)
-        {
-            if (pepega[i] == temp)
-            {
-                c = false;
-                break;
-            }
-            else
-            {
-                c = true;
-                break;
-            }
-
-        }
-        if (c)
-        {
-            pepega.push_back(temp);
-        }
-    }
-    cout << pepega.size();
-    return 0;
+	int n = 0, c = 0, b = 0;
+	bool g = true;
+	cin >> n;
+	vector<int> pepega;
+	// the processing
+	for (int i = 0; i < n; ++i)
+	{
+		int temp = 0;
+		cin >> temp;
+		for (int i = 0; i < pepega.size(); ++i)
+		{
+			if (pepega[i] == temp)
+			{
+				g = false;
+			}
+			else
+			{
+				g = true;
+			}
+		}
+		if (g) pepega.push_back(temp);	
+	}
+	cout << pepega.size();
 }
