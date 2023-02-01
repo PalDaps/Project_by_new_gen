7b51c17f7014a38bcbe87d49f0c5f6af0e677001
PalDaps <dashko.pave@bk.ru>
2023-02-01 22:27:54 +0300

    The Problem 1_7_11 first version so bad no idea

---
+++ Problem_1_7_11/Source.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+// Rearrange the adjacent elements of the vector 
+// (A[0] c A[1], A[2] c A[3], etc.). 
+// If there are an odd number of elements, 
+// then the last element remains in its place.
+// Some tests:
+
+using namespace std;
+int main()
+{
+	int n = 0;
+	cin >> n;
+	vector<int> future;
+	// the reading
+	for (int i = 0; i < n; ++i)
+	{
+		int temp = 0;
+		cin >> temp;
+		future.push_back(temp);
+	}
+	// the proccesing
+	for (int i = 2; i < n; i+=2)
+	{
+		int box = future[i - 2];
+		future[i - 2] = future[i - 1];
+		future[i - 1] = future[i - 2];
+		
+	}
+	// the outputing
+	for (int i = 0; i < n; ++i)
+	{
+		cout << future[i];
+	}
+
+	return 0;
+}
