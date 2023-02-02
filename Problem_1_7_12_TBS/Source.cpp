4d04202e60e0448745a7243a386fce6d6c2af139
PalDaps <dashko.pavel@bk.ru>
2023-02-02 19:08:05 +0300

    The bottom line is that we write numbers into cells with the index [i % n]. Let's say we have a vector of size 5. Then the first element will be written to the cell with the index [ 1 % 5 ] = [ 1 ], and the fifth element [ 5 % 5 ] = [ 0 ]. Of course, by selecting the appropriate initial values.

---
+++ Problem_1_7_12_TBS/Source.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+	int n = 0;
+	cin >> n;
+	vector<int> future(n);
+	for (int i = 1; i <= n; ++i)
+	{
+		cin >> future[i % n];
+	}
+	for (auto i : future)
+	{
+		cout << i << " ";
+	}
+	return 0;
+}
