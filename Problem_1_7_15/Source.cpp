3099f52f047286cc573f83097c0c26f51991d960
PalDaps <dashko.pavel@bk.ru>
2023-02-04 20:37:48 +0300

    The defoult solution, but it is true!

--- Problem_1_7_15/Source.cpp
+++ Problem_1_7_15/Source.cpp
@@ -1,53 +1,27 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
 
-//
-// It is known that 8 queens can be placed on an 8x8 board 
-// so that they do not beat each other. You are given an 
-// arrangement of 8 queens on the board, determine if 
-// there is a pair of them beating each other.
-//
-
-int main()
+int main() 
 {
-	int n = 8, c = 0, p = 0;
-	vector<int> x,y;
-	// the reading two vectors
-	for (int i = 0; i < n; ++i)
-	{
-		int temp_x = 0, temp_y = 0;
-		cin >> temp_x >> temp_y;
-		if (temp_x == 0 || temp_x > 8 || temp_y == 0 || temp_y > 8)
-		{
-			cout << "NO";
-			return 0;
-		}
-		x.push_back(temp_x);
-		y.push_back(temp_y);
-	}
-	// the processing two vectors
-	for (int i = 0; i < n; ++i)
-	{
-		for ( int j = 1; j < n; ++j )
-		{
-			if ((i != j) && (x[i] == x[j] && y[i] == y[j]))
-			{
-				p++;
-			}
-			if ((i != j) && (x[i] - y[i] == x[j] - y[j] || x[i] + y[i] == x[j] + y[j] || x[i] == x[j] || y[i] == y[j]))
-			{
-				c++;
-			}
-		}
-	}
-	if (c > 1 && p == 0)
-	{
-		cout << "YES";
-	}
-	else
-	{
-		cout << "NO";
-	}
-	return 0;
+    int temp;
+    bool cross = false;
+    vector <int> x, y;
+    for (int i = 1; i <= 8; i++) {
+        cin >> temp;
+        x.push_back(temp);
+        cin >> temp;
+        y.push_back(temp);
+    }
+    for (int i = 0; i < x.size() - 1; i++) {
+        for (int j = i + 1; j < x.size(); j++) {
+            if (abs(x[i] - x[j]) == abs(y[i] - y[j]) || x[i] == x[j] || y[i] == y[j]) {
+                cross = true;
+            }
+        }
+    }
+    if (cross) cout << "YES";
+    else cout << "NO";
+    return 0;
 }
