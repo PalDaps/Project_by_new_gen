c986df22c938ea64951de2dd85cf05c395043873
PalDaps <dashko.pavel@bk.ru>
2023-02-04 20:36:41 +0300

    Dont know why, but it is not true!

--- Problem_1_7_15/Source.cpp
+++ Problem_1_7_15/Source.cpp
@@ -11,13 +11,18 @@ using namespace std;
 
 int main()
 {
-	int n = 2, c = 0;
+	int n = 8, c = 0, p = 0;
 	vector<int> x,y;
 	// the reading two vectors
 	for (int i = 0; i < n; ++i)
 	{
 		int temp_x = 0, temp_y = 0;
 		cin >> temp_x >> temp_y;
+		if (temp_x == 0 || temp_x > 8 || temp_y == 0 || temp_y > 8)
+		{
+			cout << "NO";
+			return 0;
+		}
 		x.push_back(temp_x);
 		y.push_back(temp_y);
 	}
@@ -25,14 +30,18 @@ int main()
 	for (int i = 0; i < n; ++i)
 	{
 		for ( int j = 1; j < n; ++j )
-		{ 
+		{
+			if ((i != j) && (x[i] == x[j] && y[i] == y[j]))
+			{
+				p++;
+			}
 			if ((i != j) && (x[i] - y[i] == x[j] - y[j] || x[i] + y[i] == x[j] + y[j] || x[i] == x[j] || y[i] == y[j]))
 			{
 				c++;
 			}
 		}
 	}
-	if (c > 1)
+	if (c > 1 && p == 0)
 	{
 		cout << "YES";
 	}
