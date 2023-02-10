368476d56622e4bb61c91f0b9706766c3c1670dd
Admin <dashko.pavel@bk.ru>
2023-02-10 16:02:35 +0300

    The first version of prolem 1_8_3. I don't understand how to read rows evenly

--- Problem_1_8_3/Source.cpp
+++ Problem_1_8_3/Source.cpp
@@ -2,8 +2,47 @@
 #include <vector>
 using namespace std;
 
+// Find Indices of First Occurrence of Maximal Element
+
 int main()
 {
-	cout << "HI";
+	int n = 0, m = 0;
+	cin >> n >> m;
+	vector < vector<int>> vector_two(n, vector<int>(m));
+	// the reading
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < m; ++j)
+		{
+			cin >> vector_two[i][j];
+		}
+	}
+	// the processing
+	int safe = vector_two[0][0];
+	int index_i = 0;
+	int index_j = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < m; ++j)
+		{
+			if (vector_two[i - 1][j - 1] < vector_two[i][j])
+			{
+				safe = vector_two[i][j];
+				index_i = i;
+				index_j = j;
+			}
+		}
+	}
+	// the output
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < m; ++j)
+		{
+			cout << vector_two[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << "---------" << endl;
+	cout << safe << " " << index_i << " " << index_j;
 	return 0;
 }
