8ff606a2b4d2625449d461987302b4267e799980
Admin <dashko.pavel@bk.ru>
2023-02-11 17:37:18 +0300

    The problem 1_8_5 was solved! This solution is far from the fastest, but the two nested loops let me feel the power

---
+++ Problem_1_8_5/Source.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+// Given a number n not greater than 100. Create an n×n vector
+// and fill it according to the following rule. On the main diagonal, 
+// the numbers 0 should be written. On the two diagonals adjacent 
+// to the main, the numbers 1. On the next two diagonals, the numbers 
+// 2, and so on.
+
+int main()
+{
+	int n = 0;
+	cin >> n;
+	if (n < 0 || n > 100)
+	{
+		return 0;
+	}
+	vector<vector<int>> future(n, vector<int>(n));
+	// the filling ( the processing i think, because we dont give the values in vector )
+	for ( int i = 0; i < n; ++i )
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			future[i][j] = 0;
+		}
+			
+	}
+	// the processing
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			for (int k = 0; k < n; ++k)
+			{
+				if (abs(i - j) == k)
+				{
+					future[i][j] = k;
+				}
+			}
+		}
+
+	}
+	// the output
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			cout << future[i][j] << " ";
+		}
+		cout << endl;
+
+	}
+	return 0;
+}
