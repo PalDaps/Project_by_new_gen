46f93821438cd87aabc89255d04e32fef664878e
Admin <dashko.pavel@bk.ru>
2023-02-11 19:05:13 +0300

    The first version of problem 1_8_7

---
+++ Problem_1_8_7/Problem_1_8_7.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+// Check if the array is symmetrical about the main diagonal.
+//
+
+int main()
+{
+    int n = 0, c = 0;
+    cin >> n;
+    if (n < 0 || n > 10)
+    {
+        return 0;
+    }
+    vector<vector<int>> future(n, vector<int>(n));
+    // the reading
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            cin >> future[i][j];
+        }
+    }
+    // the processing
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            if (i != j && future[i][j] == future[j][i])
+            {
+                c++;
+            }
+        }
+    }
+    // the ouptut
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+           cout << future[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl <<  c;
+    return 0;
+}
