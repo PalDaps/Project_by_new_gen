8d48eadf112f59ba427f703d6ccf757a82085eda
Admin <dashko.pavel@bk.ru>
2023-02-15 00:51:57 +0300

    Solutions moved to correct file

--- Problem_1_8_10/Source.cpp
+++ Problem_1_8_10/Source.cpp
@@ -3,21 +3,23 @@
 
 using namespace std;
 
-// 
-// An n×m given - angle vector.Rotate it 90 degrees clockwise, 
-// writing the result to the new array size m×n.
-//
+// The two-dimensional vector stores information about 
+// sold tickets, the number 1 means that the ticket for 
+// this seat has already been sold, the number 0 means 
+// that the seat is free. A request has been received to 
+// sell k tickets for adjacent seats in the same row. 
+// Determine whether such a request can be fulfilled.
 
 int main()
 {
-	int n = 0, m = 0;
+	int n = 0, m = 0, k = 0, c = 0, safe = 0;
 	cin >> n >> m;
-	if (n < 0 || m < 0 || n > 100 || m > 100)
+	if (n < 0 || m < 0 || n > 20 || m > 20)
 	{
 		return 0;
 	}
 	vector<vector<int>> future(n, vector<int>(m));
-	vector<vector<int>> future_r(m, vector<int>(n));
+	vector<int> amount_null_in_line;
 	// the reading
 	for (int i = 0; i < n; ++i)
 	{
@@ -26,26 +28,43 @@ int main()
 			cin >> future[i][j];
 		}
 	}
-	// the output for norm
-	// the processing 
-	for (int i = 0; i < m; ++i)
+	// the ouput
+	for (int i = 0; i < n; ++i)
 	{
-		for (int j = 0; j < n; ++j)
+		for (int j = 0; j < m; ++j)
 		{
-			future_r[i][j] = future[j][i];
-		}
-
-	}
-	// the output
-	for (int i = 0; i < m; ++i)
-	{
-		int j = n-1;
-		while  ( j >= 0 )
-		{
-			cout << future_r[i][j] << " ";
-			j--;
+			cout << future[i][j] << " ";
 		}
 		cout << endl;
+	}
+	cin >> k;
+	// the processing 
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < m; ++j)
+		{
+			if (future[i][j] == 0)
+			{
+				c++;
+				if (c == k)
+				{
+					amount_null_in_line.push_back(i);
+				}
+			}
+			else
+			{
+				c = 0;
+			}
+		}
+		c = 0;
+	}
+	if (amount_null_in_line.size() > 0)
+	{
+		cout << amount_null_in_line[0] + 1 << " ";
+	}
+	else
+	{
+		cout << 0;
 	}
 	return 0;
 }
---
+++ Problem_1_8_11/Source.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 
+// An n×m given - angle vector.Rotate it 90 degrees clockwise, 
+// writing the result to the new array size m×n.
+//
+
+int main()
+{
+	int n = 0, m = 0;
+	cin >> n >> m;
+	if (n < 0 || m < 0 || n > 100 || m > 100)
+	{
+		return 0;
+	}
+	vector<vector<int>> future(n, vector<int>(m));
+	vector<vector<int>> future_r(m, vector<int>(n));
+	// the reading
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < m; ++j)
+		{
+			cin >> future[i][j];
+		}
+	}
+	// the output for norm
+	// the processing 
+	for (int i = 0; i < m; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			future_r[i][j] = future[j][i];
+		}
+
+	}
+	// the output
+	for (int i = 0; i < m; ++i)
+	{
+		int j = n - 1;
+		while (j >= 0)
+		{
+			cout << future_r[i][j] << " ";
+			j--;
+		}
+		cout << endl;
+	}
+	return 0;
+}
