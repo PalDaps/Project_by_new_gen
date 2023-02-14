ce612c26f1071aef3452725eeacdbc3da8604039
Admin <dashko.pavel@bk.ru>
2023-02-14 23:50:37 +0300

    The problem 1_8_10 was solved!

--- Problem_1_8_10/Source.cpp
+++ Problem_1_8_10/Source.cpp
@@ -12,14 +12,14 @@ using namespace std;
 
 int main()
 {
-	int n = 0, m = 0, k = 0, c = 1, safe = 0;
+	int n = 0, m = 0, k = 0, c = 0, safe = 0;
 	cin >> n >> m;
 	if (n < 0 || m < 0 || n > 20 || m > 20)
 	{
 		return 0;
 	}
 	vector<vector<int>> future(n, vector<int>(m));
-	vector<int> amount_null_in_line(n);
+	vector<int> amount_null_in_line;
 	// the reading
 	for (int i = 0; i < n; ++i)
 	{
@@ -37,41 +37,34 @@ int main()
 		}
 		cout << endl;
 	}
-	cout << endl << c;
 	cin >> k;
 	// the processing 
 	for (int i = 0; i < n; ++i)
 	{
-		int number_line = i;
 		for (int j = 0; j < m; ++j)
 		{
 			if (future[i][j] == 0)
 			{
-				int first_null = j;
-				int first_null_po = first_null + 1;
-				for ( first_null_po; first_null_po < m; ++first_null_po)
+				c++;
+				if (c == k)
 				{
-				 
-					if (future[i][first_null_po - 1] == future[i][first_null_po])
-					{
-					 
-						if ( number_line > safe )
-					 
-						{
-							c = 0;
-						}
-				
-					c++;
-					amount_null_in_line.push_back(c); // early pusback
-					safe = number_line;
-					}
-					else if (future[i][first_null_po - 1] != future[i][first_null_po])
-					{
-						
-					}
+					amount_null_in_line.push_back(i);
 				}
 			}
+			else
+			{
+				c = 0;
+			}
 		}
+		c = 0;
+	}
+	if (amount_null_in_line.size() > 0)
+	{
+		cout << amount_null_in_line[0] + 1 << " ";
+	}
+	else
+	{
+		cout << 0;
 	}
 	return 0;
 }
