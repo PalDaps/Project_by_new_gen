c3035c211a993ad3d8e2fb28862dde549356c766
Admin <dashko.pavel@bk.ru>
2023-02-15 00:48:24 +0300

    The problem 1_8_10 was solved

--- Problem_1_8_10/Source.cpp
+++ Problem_1_8_10/Source.cpp
@@ -1,25 +1,23 @@
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 
 using namespace std;
 
-// The two-dimensional vector stores information about 
-// sold tickets, the number 1 means that the ticket for 
-// this seat has already been sold, the number 0 means 
-// that the seat is free. A request has been received to 
-// sell k tickets for adjacent seats in the same row. 
-// Determine whether such a request can be fulfilled.
+// 
+// An n×m given - angle vector.Rotate it 90 degrees clockwise, 
+// writing the result to the new array size m×n.
+//
 
 int main()
 {
-	int n = 0, m = 0, k = 0, c = 0, safe = 0;
+	int n = 0, m = 0;
 	cin >> n >> m;
-	if (n < 0 || m < 0 || n > 20 || m > 20)
+	if (n < 0 || m < 0 || n > 100 || m > 100)
 	{
 		return 0;
 	}
 	vector<vector<int>> future(n, vector<int>(m));
-	vector<int> amount_null_in_line;
+	vector<vector<int>> future_r(m, vector<int>(n));
 	// the reading
 	for (int i = 0; i < n; ++i)
 	{
@@ -28,43 +26,26 @@ int main()
 			cin >> future[i][j];
 		}
 	}
-	// the ouput
-	for (int i = 0; i < n; ++i)
+	// the output for norm
+	// the processing 
+	for (int i = 0; i < m; ++i)
 	{
-		for (int j = 0; j < m; ++j)
+		for (int j = 0; j < n; ++j)
 		{
-			cout << future[i][j] << " ";
+			future_r[i][j] = future[j][i];
+		}
+
+	}
+	// the output
+	for (int i = 0; i < m; ++i)
+	{
+		int j = n-1;
+		while  ( j >= 0 )
+		{
+			cout << future_r[i][j] << " ";
+			j--;
 		}
 		cout << endl;
-	}
-	cin >> k;
-	// the processing 
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < m; ++j)
-		{
-			if (future[i][j] == 0)
-			{
-				c++;
-				if (c == k)
-				{
-					amount_null_in_line.push_back(i);
-				}
-			}
-			else
-			{
-				c = 0;
-			}
-		}
-		c = 0;
-	}
-	if (amount_null_in_line.size() > 0)
-	{
-		cout << amount_null_in_line[0] + 1 << " ";
-	}
-	else
-	{
-		cout << 0;
 	}
 	return 0;
 }
