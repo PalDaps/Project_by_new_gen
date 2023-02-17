2c9ad0767f4b3df850ac63887adae0d6a6bf4cb7
Admin <dashko.pavel@bk.ru>
2023-02-17 13:14:30 +0300

    The first version of problem 1_8_15

--- Problem_1_8_15/Source.cpp
+++ Problem_1_8_15/Source.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 int main()
 {
-	int n = 0, m = 0, c = 0, b = 1, moove = 0;
+	int n = 0, m = 0, c = 1, b = 1, moove = 1;
 	cin >> n >> m;
 	vector<vector<int>> future(n, vector<int>(m));
 	vector<vector<int>> future_r(n, vector<int>(m));
@@ -22,38 +22,36 @@ int main()
 	{
 		for (int j = 0; j < m; ++j)
 		{
-			future_r[i][j] = b;
-			b++;
+			future_r[i][j] = 0;
 		}
 	}
 	// the processing
 	int safe_i = 0;
-	if ( moove == 1 ) 
+	for ( moove; moove<3; moove++ )
 	{ 
-	    for (int i = 0; i < n; ++i)
-	    {
-	    	for (int j = 0; j < m; ++j)
-	    	{
-				future[i][j] = c;
-				c++;
-				safe_i = j;
-	    	}
-			moove = 2;
-			break;
+	    if ( moove == 1 ) 
+	    { 
+	        for (int i = 0; i < n; ++i)
+	        {
+	        	for (int j = 0; j < m; ++j)
+	        	{
+	    			future[i][j] = c;
+	    			c++;
+	    			safe_i = j;
+	        	}
+	    		moove = 2;
+	    		break;
+	        }
 	    }
-	}
-	if (moove == 2)
-	{
-		for (int i = 0; i < n; ++i)
-		{
-			for (int j = 0; j < m; ++j)
-			{
-				future[j][safe_i] = c;
-				c++;
-			}
-			moove = 2;
-			break;
-		}
+	    if (moove == 2)
+	    {
+	    	for (int j = 0; j < n-1; ++j)
+	    	{
+	    		future[j][safe_i] = c;
+	    		c++;
+	    	}
+	    	moove = 2;
+	    }
 	}
 
 
@@ -63,8 +61,9 @@ int main()
 	{
 		for (int j = 0; j < m; ++j )
 		{
-			
+			cout << setw(3) << future[i][j] << " ";
 		}
+		cout << endl;
 	}
 
 	
