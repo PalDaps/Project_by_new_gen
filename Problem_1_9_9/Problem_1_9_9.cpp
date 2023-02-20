ae60891d882ec53ef2d3ee2795c3ea605cf4ab97
Admin <dashko.pavel@bk.ru>
2023-02-20 21:04:50 +0300

    The problem 1_9_9 was solved!

---
+++ Problem_1_9_9/Problem_1_9_9.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+//
+// Given a real positive number a and an integer n.
+// Calculate a to the power of n.Formalize the solution 
+// in the form of a recursive function power(a, n).
+//
+double pow(double a, int n)
+{
+    double pow_1 = a;
+    if (n > 0)
+    {
+        for (int i = 1; i < n; ++i)
+        {
+            pow_1 = a * pow_1;
+        }
+        return pow_1;
+    }
+    else if (n == 0) return 1;
+    else return 1 / (pow(a, abs(n)));
+}
+int main()
+{
+    int n = 0;
+    double a = 0;
+    std::cin >> a >> n;
+    std::cout << pow(a,n);
+    return 0;
+}
