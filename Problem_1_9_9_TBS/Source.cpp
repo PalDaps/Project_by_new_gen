2aa940a7ad7803d53f47f64c44bbd55da2c6b921
Admin <dashko.pavel@bk.ru>
2023-02-23 14:47:26 +0300

    A clean solution to the problem of raising a to the power of n using recursion. Found a stackoverflow bug, fixed it.

--- Problem_1_9_9_TBS/Source.cpp
+++ Problem_1_9_9_TBS/Source.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 
-double pow(double a, int n);
+double pow(double a, int n)
 {
-
+	if (n == 0) return 1;
+	else if (n > 0) return a * pow(a, n - 1);
+	else return 1 / (a * pow(a, abs(n) - 1));
 }
 int main()
 {
 	double a = 0;
 	int n = 0;
 	std::cin >> a >> n;
+	std::cout << pow(a, n);
 	return 0;
 }
