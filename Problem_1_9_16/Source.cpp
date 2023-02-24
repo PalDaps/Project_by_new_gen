939faa96e6090a7e2620c8b7c3fbefc80937a45f
Admin <dashko.pavel@bk.ru>
2023-02-24 21:33:50 +0300

    The preparation for this hard problem!

---
+++ Problem_1_9_16/Source.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+//
+// Given a number N. Determine in how many ways 
+// there are N × N N queens on the board that do not 
+// attack each other.
+//
+
+int main()
+{
+
+	return 0;
+}
