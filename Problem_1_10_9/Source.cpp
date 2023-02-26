e0e4a1fdb606b336d0f77f889c988971a09fc636
Admin <dashko.pavel@bk.ru>
2023-02-27 00:13:07 +0300

    The second version of problem 1_10_9

--- Problem_1_10_9/Source.cpp
+++ Problem_1_10_9/Source.cpp
@@ -10,6 +10,7 @@
 int main()
 {
 	int pointPosition = 0, numbOfPoints = 0, indOfBegNumb = 0;
+	int codeNumberNull, codeNumberOne, codeNumberTwo, codeNumberThree;
 	std::vector<int> fourPosPoint;
 	std::string ipAddres = "255.255.255.255", number_null = "", number_one = "", number_two = "", number_three = "";
 	getline(std::cin, ipAddres);
@@ -21,30 +22,59 @@ int main()
 		pointPosition++;
 		numbOfPoints++;	
 	}
+	if (numbOfPoints != 3)
+	{
+		std::cout << "NO";
+		return 0;
+	}
+	else if (fourPosPoint[0] == 0 || fourPosPoint[2] == ipAddres.size()-1)
+	{
+		std::cout << "NO";
+		return 0;
+	}
 	for (indOfBegNumb; indOfBegNumb < fourPosPoint[0]; indOfBegNumb++) // looking for the number up to the first point
 	{
 		number_null = number_null + ipAddres[indOfBegNumb];
+	}
+	codeNumberNull = stoi(number_null);
+	if (codeNumberNull < 0 || codeNumberNull >255)
+	{
+		std::cout << "NO";
+		return 0;
 	}
 	indOfBegNumb = fourPosPoint[0] + 1;
 	for (indOfBegNumb; indOfBegNumb < fourPosPoint[1]; indOfBegNumb++) // looking for the number up to the first point
 	{
 		number_one = number_one + ipAddres[indOfBegNumb];
 	}
+	codeNumberOne = stoi(number_one);
+	if (codeNumberNull < 0 || codeNumberNull >255)
+	{
+		std::cout << "NO";
+		return 0;
+	}
 	indOfBegNumb = fourPosPoint[1] + 1;
 	for (indOfBegNumb; indOfBegNumb < fourPosPoint[2]; indOfBegNumb++) // looking for the number up to the first point
 	{
 		number_two = number_two + ipAddres[indOfBegNumb];
+	}
+	codeNumberTwo = stoi(number_null);
+	if (codeNumberTwo < 0 || codeNumberNull >255)
+	{
+		std::cout << "NO";
+		return 0;
 	}
 	indOfBegNumb = fourPosPoint[2] + 1;
 	for (indOfBegNumb; indOfBegNumb < ipAddres.size(); indOfBegNumb++) // looking for the number up to the first point
 	{
 		number_three = number_three + ipAddres[indOfBegNumb];
 	}
-	int codeNumberNull, codeNumberOne, codeNumberTwo, codeNumberThree;
-	codeNumberNull = stoi(number_null);
-	codeNumberOne = stoi(number_one);
-	codeNumberTwo = stoi(number_two);
-	codeNumberThree = stoi(number_three);
+	codeNumberThree = stoi(number_null);
+	if (codeNumberThree < 0 || codeNumberNull >255)
+	{
+		std::cout << "NO";
+		return 0;
+	}
 	std::cout << codeNumberNull << " " << codeNumberOne << " " << codeNumberTwo << " " << codeNumberThree << " " << std::endl;
 	std::cout << number_null << " " << number_one << " " << number_two << " " << number_three << std::endl;
 	std::cout << numbOfPoints;
