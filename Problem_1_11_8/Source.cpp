a9be92c6364f45bd6fe2987a2dc890ee49301933
Pavel Dashko <dashko.pavel@bk.ru>
2023-06-17 01:23:04 +0300

    Not yet

--- Problem_1_11_8/Source.cpp
+++ Problem_1_11_8/Source.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <map>
-#include <string>
 #include <set>
-#include <iterator>
 #include <vector>
+#include <string>
 
 //
 // Creating a Latin-English dictionary from English-Latin.
@@ -25,8 +24,8 @@ std::vector<std::string> splitLine(std::string& line) {
 	return result;
 }
 
-std::multiset<std::string> InOrderLat(std::vector<std::string>& mem) {
-	std::multiset<std::string> result;
+std::set<std::string> InOrderLat(std::vector<std::string>& mem) {
+	std::set<std::string> result;
 	for (int i = 0; i < mem.size(); i++) {
 		std::vector<std::string> oneEngLats = splitLine(mem[i]);
 		for (int j = 1; j < oneEngLats.size(); j++) {
@@ -51,6 +50,22 @@ std::map<std::string, std::vector<std::string>> splitEngLat(std::vector<std::str
 	return result;
 }
 
+std::multimap<std::string, std::vector<std::string>> LatEng(std::map<std::string, std::vector<std::string>>& result, std::set<std::string> lat) {
+	std::multimap<std::string, std::vector<std::string>> LatEng;
+	for (const auto& j : lat) {
+		std::string key = j;
+		std::vector<std::string> not_key;
+		for (const auto& i : result) {
+			const std::vector<std::string>& values = i.second;
+			if (std::find(values.begin(), values.end(), j) != values.end()) {
+				not_key.push_back(i.first);
+			}
+		}
+		LatEng.insert(std::make_pair(key, not_key));
+	}
+	return LatEng;
+}
+
 int main()
 {
 	int number_of_word = 0;
@@ -62,19 +77,51 @@ int main()
 	map.insert(std::make_pair("punishment", "- malum, multa"));
 	for (const auto& i : map)
 		std::cout << i.first << " " << i.second << std::endl;*/
-	/*std::cin >> number_of_word;
+	std::cin >> number_of_word;
 	std::cin.ignore(1, '\n');
 	for (int i = 1; i < number_of_word+1; i++) {
 		std::getline(std::cin, line);
 		mem.push_back(line);
-	}*/
+	}
 	/*for (const auto& i : mem)
 		std::cout << i << std::endl;*/
-	mem.push_back("apple - malum, pomum, popula");
+	/*mem.push_back("apple - malum, pomum, popula");
 	mem.push_back("fruit - baca, bacca, popum");
-	mem.push_back("punishment - malum, multa");
-	// splitLine(mem[0]);
-	InOrderLat(mem);
-	splitEngLat(mem);
+	mem.push_back("punishment - malum, multa");*/
+	//std::map<std::string, std::vector<std::string>> result = splitEngLat(mem);
+	//std::set<std::string> lat = InOrderLat(mem);
+	//// splitLine(mem[0]);
+	//
+	//for (const auto& j : lat) {
+	//	std::cout << j << " - ";
+	//	for (const auto& i : result) {
+	//		const std::vector<std::string>& values = i.second;
+	//		if (std::find(values.begin(), values.end(), j) != values.end()) {
+	//			std::cout << i.first << " ";
+	//		}
+	//	}
+	//	std::cout << std::endl;
+	//}
+	std::map<std::string, std::vector<std::string>> result = splitEngLat(mem);
+	std::set<std::string> lat = InOrderLat(mem);
+	std::multimap<std::string, std::vector<std::string>> Finish = LatEng(result, lat);
+
+	for (const auto& i : Finish) {
+		std::cout << i.first << " - ";
+		int counter = 0;
+		for (const auto& j : i.second) {
+			if (i.second.size() > 1) {
+				if (counter < i.second.size() - 1) {
+					std::cout << j << ", ";
+					counter++;
+				}
+				else std::cout << j;
+			}
+			else
+				std::cout << j << " ";
+		}
+		std::cout << std::endl;
+	}
+
 	return 0;
 }
